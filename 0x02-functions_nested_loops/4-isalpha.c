b8b8bc5de4f3293b7dccdea18aaa167bc5cf021e
reda3ZI <redazoubai49@gmail.com>
2023-03-22 17:26:22 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,18 +2,16 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anyting else
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
-	else
 	{
 		return (0);
 	}
-	_putchar('\n');
 }

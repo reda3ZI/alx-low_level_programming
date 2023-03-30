ed55e931d9be422202eb4ee1b5eb22f30fcb2463
reda3ZI <redazoubai49@gmail.com>
2023-03-30 15:41:10 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * leet - encodes a string into 1337
+  * @s: string
+  *
+  * Return: string result
+  */
+char *leet(char *s)
+{
+	int a, b;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (s[a] == s1[b])
+			{
+				s[a] = s2[b];
+			}
+		}
+	}
+	return (s);
+}

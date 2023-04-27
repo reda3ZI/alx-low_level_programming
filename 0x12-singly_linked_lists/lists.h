b2aa243f9c7067829b2b4dada3bc825dc04ead03
reda3ZI <redazoubai49@gmail.com>
2023-04-27 09:58:24 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,18 @@
 #define LISTS_H
 
 /**
-  * struct list_s - singly linked list
-  * @str: string
-  * @len: lengh of the string
-  * @next: points to the next node
-  * Description: singly linked list node structure
-  */
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}
-list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
